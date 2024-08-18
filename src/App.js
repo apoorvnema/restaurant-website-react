c0d05615c70551cc0dae399998773fda08376e39
apoorvnema <apoorvnema38@gmail.com>
2024-08-18 16:35:44 +0530

    [NEW]: Added Meals Summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from "./components/Layout/Header";
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
     <>
-      <Header/>
+      <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
