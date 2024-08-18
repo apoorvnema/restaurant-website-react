851698cc46b6a7f1a5de4a2ab8eeb1e1e2efcc59
apoorvnema <apoorvnema38@gmail.com>
2024-08-18 17:22:38 +0530

    [NEW]: Added Cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import cartIcon from '../../assets/cartIcon.svg'
 import classes from './HeaderCartButton.module.css'
+import CartIcon from '../Cart/CartIcon'
 
 const HeaderCartButton = (props) => {
     return (
         <button className={classes.button}>
             <span className={classes.icon}>
-            <img style={{width:25}} src={cartIcon} alt="Cart" />
+            <CartIcon />
             </span>
             <span>Your Cart</span>
             <span className={classes.badge}>
