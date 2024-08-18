04ab01dd2ba57da9de4d5c4a1d4fde6ecf60d3f3
apoorvnema <apoorvnema38@gmail.com>
2024-08-18 17:32:44 +0530

    [UPDATE]: Cart Open & Close

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = props => {
     return <>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='Meals'/>
